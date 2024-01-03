2c64ac21495624b5a4caabb13b1b5722c6a57faf
Celienn <81368874+Celienn@users.noreply.github.com>
2024-01-03 19:53:45 +0100

    Update consoleLog.js

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -1,4 +1,4 @@
 module.exports = (client) => {
-    console.log(`✅ ${process.env.CLIENT_NAME} logged !`);
+    console.log(`✅ ${process.env.CLIENT_NAME} logged !!`);
     client.user.setActivity("checking next stock")
 };
